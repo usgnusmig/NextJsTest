de7ae1a606d9141f79a3b0bf31bf9dda19472baf
usgnusmig <usgnusmig@gmail.com>
2022-03-01 15:20:00 +0900

    fix layout

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 const HeaderItem = ({ Icon, title, link }) => {
   return (
     <Link href={link}>
-      <a className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+      <a className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-black">
         <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-        <p className="opacity-0 group-hover:opacity-100 tracking-widset">
+        <p className="opacity-50 group-hover:opacity-100 tracking-widset">
           {title}
         </p>
       </a>
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import "../styles/globals.css";
+import Layout from "../components/Layout";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      {/* 헤더 */}
-      <Header />
-
-      {/* 컨텐츠들 */}
+    <Layout>
       <Component {...pageProps} />
-    </div>
+    </Layout>
   );
 }
 
